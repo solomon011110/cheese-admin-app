f246d7b78c8a39bcdd15ee799efa45c295babbbf
敷島宙 <sorasion1205@gmail.com>
2025-06-23 10:52:38 +0900

    comf_main

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Cheese for Admin",
   description: "管理者用アプリ",
 };
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -24,7 +12,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
+        <header></header>
         {children}
       </body>
     </html>
